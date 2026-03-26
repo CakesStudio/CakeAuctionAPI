6a713642c9578659f0f947ae841709ec9e8657a6
N1f-Angel <160177189+N1f-Angel@users.noreply.github.com>
2026-03-26 00:59:25 +0000

    Release v1.3.7, add runAction method

--- src/main/java/dev/cakestudio/cakeauctionapi/api/manager/action/IActionManager.java
+++ src/main/java/dev/cakestudio/cakeauctionapi/api/manager/action/IActionManager.java
@@ -4,6 +4,9 @@ import dev.cakestudio.cakeauctionapi.api.action.IAction;
 
 import lombok.NonNull;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
 /**
  * Manages custom actions that can be triggered from configuration files.
  */
@@ -25,5 +28,15 @@ public interface IActionManager {
      * @param tag The tag.
      */
     void unregisterAction(@NonNull String tag);
+
+    /**
+     * Runs an action for a player.
+     *
+     * @param player   The player who triggers the action.
+     * @param location The location context for the action (e.g., sound source).
+     * @param tag      The action tag (e.g., "OPEN_MENU").
+     * @param data     The data for the action (e.g., "main-menu").
+     */
+    void runAction(@NonNull Player player, @NonNull Location location, @NonNull String tag, @NonNull String data);
 
 }
